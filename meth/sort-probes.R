ce08061e35f8cf9ab19fa27e9bce05eed38df90d
Árni Johnsen <arni.johnsen@gmail.com>
2015-05-29 10:42:27 +0000

    Changed methylation probe sorting script to use data.table

--- meth/sort-probes.R
+++ meth/sort-probes.R
@@ -1,33 +1,38 @@
+library(data.table)
+
+cancer.type <- "brca"
+raw.data.dir <- "/share/scratch/arj32/raw-data/"
+parsed.data.dir <- "/share/scratch/arj32/parsed-data/"
+
 cat("Reading probes annotation file..\n")
-probe.annotations <- read.table("../rawdata/AnnotationFiles/GenomeStudioProbeAnnotations.txt", header=TRUE, sep="\t", quote="\"", stringsAsFactors=FALSE)
+probe.ann <- fread(paste(raw.data.dir, 
+                         "annotation-files/GenomeStudioProbeAnnotations.txt",
+                         sep=""),
+                    select=c(2,14, 16,19))
+setnames(probe.ann, c("probe", "group", "cpg.island", "is.enhancer"))
 
 cat("Setting probe status..\n")
-probe.annotations$status <- 0
+# Creating logical vectors which indicate which probe is in which category -----
+probe.ann[,is.body  :=!grepl("TSS200|TSS1500|1stExon|5'UTR|3'UTR|^$", group)]
+probe.ann[,is.island:= grepl("Island", cpg.island)]
+probe.ann[,is.shore := grepl("Shore",  cpg.island)]
+probe.ann[,is.none  :=(!is.island & !is.shore)]
+probe.ann[,is.promoter:=grepl("TSS200|5'UTR", group)]
+probe.ann[is.na(is.enhancer)]$is.enhancer <- FALSE
 
-cat("  ..gene bodies\n")
-is.body   <- !grepl("TSS200|TSS1500|1stExon|5'UTR|3'UTR|^$", probe.annotations$UCSC_REFGENE_GROUP)
-is.island <-  grepl("Island", probe.annotations$RELATION_TO_UCSC_CPG_ISLAND)
-is.shore  <-  grepl("Shore",  probe.annotations$RELATION_TO_UCSC_CPG_ISLAND)
-is.none   <- !is.island & !is.shore
-probe.annotations$status[is.body & is.island] <- "body.island"
-probe.annotations$status[is.body & is.shore]  <- "body.shore"
-probe.annotations$status[is.body & is.none]   <- "body.none"
+# Creating status column, with default case (undefined) and then adding other --
+# status types in increasing order of importance (most important is laste) -----
+probe.ann[,status:="undefined"]
+probe.ann[is.body & is.island]$status <- "body.island"
+probe.ann[is.body & is.shore ]$status <- "body.shore"
+probe.ann[is.body & is.none  ]$status <- "body.none"
+probe.ann[(is.enhancer)      ]$status <- "enhancer"
+probe.ann[(is.promoter)      ]$status <- "promoter"
 
-cat("  ..enhancers\n")
-probe.annotations$status[probe.annotations$ENHANCER] <- "enhancer"
-cat("  ..promoters\n")
-probe.annotations$status[grepl("TSS200|5'UTR", probe.annotations$UCSC_REFGENE_GROUP)] <- "promoter"
-
-
-
-body.island.probes <- probe.annotations$TargetID[probe.annotations$status == "body.island"]
-body.shore.probes  <- probe.annotations$TargetID[probe.annotations$status == "body.shore" ]
-body.none.probes   <- probe.annotations$TargetID[probe.annotations$status == "body.none"  ]
-enhancer.probes    <- probe.annotations$TargetID[probe.annotations$status == "enhancer"   ]
-promoter.probes    <- probe.annotations$TargetID[probe.annotations$status == "promoter"   ]
-
-sorted.probes <- list(bi = body.island.probes, bs = body.shore.probes, bn = body.none.probes, en = enhancer.probes, pr = promoter.probes)
-
-# Save as .Rdata
-save(sorted.probes, file="../parsed-data/BRCA/annotation/sorted-probes.Rdata")
-quit(save="no")
+cat("Writing to file..\n")
+write.table(probe.ann[,c(1,10),with=F],
+            paste(parsed.data.dir,
+                  cancer.type,
+                  "/info/meth-probe-status.txt",
+                  sep=""),
+            quote=F, row.names=F)
