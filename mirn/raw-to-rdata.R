0914425bcb42fc6dd8aa3b512bb6b00c452b8ed3
Árni Johnsen <arni.johnsen@gmail.com>
2015-06-01 14:14:41 +0000

    Added script to sort samples and read data for miRNAsequencing

---
+++ mirn/raw-to-rdata.R
@@ -0,0 +1,66 @@
+library(data.table)
+
+# Define cancer type, raw and parsed data directories --------------------------
+cancer.type <- "brca"
+raw.data.dir    <- "/share/scratch/arj32/raw-data/"
+parsed.data.dir <- "/share/scratch/arj32/parsed-data/"
+source.file.path<- "/mirn/miRNASeq/BCGSC__IlluminaHiSeq_miRNASeq/Level_3/"
+source.file.dir <- paste(raw.data.dir, cancer.type, source.file.path, sep="")
+output.dir      <- paste(parsed.data.dir, cancer.type, "/mirn/",  sep="")
+
+# Load file and sample information ---------------------------------------------
+file.sample.map <- fread(paste(raw.data.dir, cancer.type,
+                               "/mirn/FILE_SAMPLE_MAP.txt", sep=""))
+setnames(file.sample.map, c("filename", "barcode"))
+# use only genes.normalized_results
+file.sample.map <- file.sample.map[grep("mirna.quantification", filename)]
+
+sample.list <- fread(paste(parsed.data.dir, cancer.type,
+                               "/info/mirn-participants.txt", sep=""))
+
+sample.list[normal.barcode == ""]$normal.barcode <- NA
+
+n <- dim(sample.list)[1]
+
+# Read one file with all information, use to create data.tables ----------------
+cat("Initiating tables..\n")
+idx.cancer <- which(!is.na(sample.list$cancer.barcode))[1]
+idx.normal <- which(!is.na(sample.list$normal.barcode))[1]
+cancer.barcode <- sample.list$cancer.barcode[idx.cancer]
+normal.barcode <- sample.list$normal.barcode[idx.normal]
+cancer.file <- file.sample.map[barcode==cancer.barcode, filename]
+normal.file <- file.sample.map[barcode==normal.barcode, filename]
+
+cat(paste(source.file.dir, cancer.file, sep=""), "\n")
+cat(paste(source.file.dir, normal.file, sep=""), "\n")
+
+cancer.wide <- fread(paste(source.file.dir, cancer.file, sep=""),select=1)
+normal.wide <- fread(paste(source.file.dir, normal.file, sep=""),select=1)
+
+setnames(cancer.wide, c("mirna"))
+setnames(normal.wide, c("mirna"))
+
+# Read all files, drop everything except beta and append as a new column--------
+for(i in 1:n){
+  cat("Reading files for participant", i, "of", n, "\n")
+  cancer.barcode <- sample.list$cancer.barcode[i]
+  normal.barcode <- sample.list$normal.barcode[i]
+  cancer.file <- file.sample.map[barcode==cancer.barcode, filename]
+  normal.file <- file.sample.map[barcode==normal.barcode, filename]
+  if(!is.na(cancer.barcode)){
+    tmp <- fread(paste(source.file.dir, cancer.file, sep=""), select=3)
+    cancer.wide[,c(cancer.barcode):=tmp]
+  }
+  if(!is.na(normal.barcode)){
+    tmp <- fread(paste(source.file.dir, normal.file, sep=""), select=3)
+    normal.wide[,c(normal.barcode):=tmp]
+  }
+}
+
+# Assign systematic names to data frames and save ------------------------------
+assign(paste(cancer.type, ".mirn.cancer", sep=""), cancer.wide)
+assign(paste(cancer.type, ".mirn.normal", sep=""), normal.wide)
+save(list = paste(cancer.type, ".mirn.cancer", sep=""),
+     file = paste(output.dir, cancer.type, "-mirn-cancer.Rdata", sep=""))
+save(list = paste(cancer.type, ".mirn.normal", sep=""),
+     file = paste(output.dir, cancer.type, "-mirn-normal.Rdata", sep=""))
---
+++ mirn/sort-samples.R
@@ -0,0 +1,48 @@
+library(data.table)
+
+cancer.type <- "brca"
+raw.data.dir    <- "/share/scratch/arj32/raw-data/"
+parsed.data.dir <- "/share/scratch/arj32/parsed-data/"
+
+file.sample.map <- fread(paste(raw.data.dir, cancer.type, "/mirn/FILE_SAMPLE_MAP.txt", sep=""))
+
+# use only genes.normalized_results
+file.sample.map <- file.sample.map[grep("mirna.quantification", filename)]
+
+# dont use recurrent cancers or files with multiple barcodes
+bad.barcodes <- grepl(",", file.sample.map$barcode) | (substr(file.sample.map$barcode, 14, 15) == "06")
+#bad.barcodes <- grepl(",", file.sample.map$barcode)
+file.sample.map <- file.sample.map[!bad.barcodes]
+
+database.info <- data.table(participant = unique(substr(file.sample.map$barcode, 1, 12)))
+
+# Find cancer barcode
+cancer.barcode <- sapply(database.info$participant, function(x){tmp <- grep(paste(x,"-01",sep=""), file.sample.map$barcode)
+                                                             if(length(tmp) > 1){
+                                                               warning("Sample has more than 1 cancer")
+                                                               tmp <- tmp[1]
+                                                             }
+                                                             if(length(tmp) > 0){
+                                                               file.sample.map$barcode[tmp]
+                                                             } else {
+                                                               NA
+                                                             }
+                                                             })
+database.info$cancer.barcode <- unlist(cancer.barcode)
+
+# Find normal barcode
+normal.barcode <- sapply(database.info$participant, function(x){tmp <- grep(paste(x,"-1",sep=""), file.sample.map$barcode)
+                                                             if(length(tmp) > 1){
+                                                               warning("Sample has more than 1 normal")
+                                                               tmp <- tmp[1]
+                                                             }
+                                                             if(length(tmp) > 0){
+                                                               file.sample.map$barcode[tmp]
+                                                             } else {
+                                                               NA
+                                                             }
+                                                             })
+database.info$normal.barcode <- unlist(normal.barcode)
+
+write.table(database.info, paste(parsed.data.dir, cancer.type, "/info/mirn-participants.txt", sep=""), quote=F, row.names=F)
+
