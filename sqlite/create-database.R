3358e0aa56f81cadd536e58f1363049ac6bb36d4
Árni Johnsen <arni.johnsen@gmail.com>
2015-06-03 09:17:50 +0000

    Added muta and mirn to SQLite database

--- sqlite/create-database.R
+++ sqlite/create-database.R
@@ -5,28 +5,36 @@ cancer.type <- "brca"
 raw.data.dir    <- "/share/scratch/arj32/raw-data/"
 parsed.data.dir <- "/share/scratch/arj32/parsed-data/"
 
-# Open database
-db <- dbConnect(SQLite(), dbname=paste(parsed.data.dir, cancer.type, "/", cancer.type, ".sqlite", sep=""))
+# Open connection to database --------------------------------------------------
+db <- dbConnect(SQLite(), dbname=paste(parsed.data.dir, cancer.type, "/",
+                                       cancer.type, ".sqlite", sep=""))
 
-# Check if db contains anything
+# Check if db contains anything ------------------------------------------------
 if(length(dbListTables(db)) != 0){
   stop("Database isn't empty\n")
 }
 
-data.types <- c("cnvw", "expr", "meth")
-data.dirs  <- c("cnv",  "expr", "meth")
-for(i in 1:3){
+# Read data.tables one by one and add them to SQLite database ------------------
+data.types <- c("cnvw", "expr", "meth", "muta", "mirn")
+data.dirs  <- c("cnv",  "expr", "meth", "muta", "mirn")
+for (i in 1:length(data.types)){
   cat("Adding", data.types[i], "data..\n")
-  cat("..normal\n")
-  cat("....reading\n")
-  load(paste(parsed.data.dir, cancer.type, "/", data.dirs[i], "/", cancer.type, "-", data.types[i], "-normal.Rdata", sep=""))
-  cat("....writing\n")
-  dbWriteTable(conn  = db,
-               name  = paste(data.types[i], "_normal", sep=""),
-               value = get(paste(cancer.type, data.types[i], "normal", sep=".")),
-               row.names = F)
-  rm(list=paste(cancer.type, data.types[i], "normal", sep="."))
+  for (j in c("cancer", "normal")){
+    if(!(data.types[i] == "muta" & j == "normal")){
+      cat("..", j, "\n", sep="")
+      cat("....reading\n")
+      load(paste(parsed.data.dir, cancer.type, "/", data.dirs[i], "/",
+                 cancer.type, "-", data.types[i], "-", j, ".Rdata", sep=""))
+      cat("....writing\n")
+      dbWriteTable(conn  = db,
+                   name  = paste(data.types[i], "_", j, sep=""),
+                   value = get(paste(cancer.type, data.types[i], j, sep=".")),
+                   row.names = F)
+      rm(list=paste(cancer.type, data.types[i], j, sep="."))
+      gc()
+    }
+  }
 }
 
-#cat(dbListTables(db))
+# Close connection to database -------------------------------------------------
 dbDisconnect(db)
