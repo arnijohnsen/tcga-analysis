3e8e8272792208ba49f590f67808f720ae6d83a6
Árni Johnsen <arni.johnsen@gmail.com>
2015-06-11 09:25:00 +0000

    Deleted old sort sample files, refactored cnvs reading script

--- read/read_cnvs.R
+++ read/read_cnvs.R
@@ -1,52 +1,114 @@
 library(data.table)
+library(yaml)    # Read config
+library(utils)   # Progress bar
+library(R.utils) # Verbose
+library(CNTools) # Convert from long to wide
 
-# Define cancer type, raw and parsed data directories --------------------------
-cancer_type <- "brca"
-raw_data_dir    <- "/share/scratch/arj32/raw_data/"
-parsed_data_dir <- "/share/scratch/arj32/parsed_data/"
-source_file_path<- "/cnvs/CNV_SNP_Array/BI__Genome_Wide_SNP_6/Level_3/"
-source_file_dir <- paste(raw_data_dir, cancer_type, source_file_path, sep="")
-output_dir      <- paste(parsed_data_dir, cancer_type, "/cnvs/",  sep="")
+# Define verbose and verbose level
+verbose <- Verbose(threshold = -1)
 
-# Load file and sample information ---------------------------------------------
-file_sample_map <- fread(paste(raw_data_dir, cancer_type,
-                               "/cnvs/FILE_SAMPLE_MAP.txt", sep=""))
+# Read base directories from config.yml, define cancer and data type -----------
+header(verbose, "Reading copy number variation data", padding = 0)
+enter(verbose, "Preparation")
+cat(verbose, "Reading config.yml")
+config <- yaml.load_file("/share/scratch/arj32/tcga-analysis/config.yml")
+raw_data_dir    <- config$data_dirs$raw_data
+parsed_data_dir <- config$data_dirs$parsed_data
+cancer_type     <- config$cancer_type
+data_type       <- "cnvs"
+
+# Define absolute directories --------------------------------------------------
+cat(verbose, "Defining directories to read from")
+file_filter     <- "nocnv_hg19"
+src_file_path <- "/CNV_SNP_Array/BI__Genome_Wide_SNP_6/Level_3/"
+src_file_dir <- paste(
+  raw_data_dir, cancer_type, "/", data_type, src_file_path, sep = ""
+)
+output_dir <- paste(parsed_data_dir, cancer_type, "/", data_type, "/",  sep="")
+
+# Generate list of files to read and barcodes of used samples ------------------
+cat(verbose, "Generating list of files to read")
+file_sample_map <- fread(
+  paste(
+    raw_data_dir, cancer_type, "/", data_type, "/FILE_SAMPLE_MAP.txt", sep = ""
+  )
+)
 setnames(file_sample_map, c("filename", "barcode"))
-# Use only nocsv_hg19
-file_sample_map <- file_sample_map[grep("nocnv_hg19", filename)]
+file_sample_map <- file_sample_map[grep(file_filter, filename)]
+setkey(file_sample_map, barcode)
+all_participants <- fread(
+  paste(
+    parsed_data_dir, cancer_type, "/info/participants.txt", sep = ""
+  )
+)
+participants <- unlist(
+  all_participants[, grep(data_type, names(all_participants)), with = F]
+)
+participants <- participants[!is.na(participants)]
+exit(verbose)
 
-sample_list <- fread(paste(parsed_data_dir, cancer_type,
-                           "/info/cnvs_participants.txt", sep=""))
+# Create empty lists where data frames are added -------------------------------
+enter(verbose, "Reading source files")
+cat(verbose, "Creating empty lists")
+cnvs_list <- list()
 
-n <- dim(sample_list)[1]
-
-# Create empty lists -----------------------------------------------------------
-cancer_list <- list()
-normal_list <- list()
-
-# Read files and create a list of data.tables ----------------------------------
+# Reading files one by one and adding to list ----------------------------------
+cat(verbose, "Reading files from", src_file_dir)
+cat(verbose, "Progress:")
+n <- length(participants)
+pb <- txtProgressBar(max = n, style = 3)
 for(i in 1:n){
-  cat("Reading files for participant", i, "of", n, "\n")
-  cancer_barcode <- sample_list$cancer_barcode[i]
-  normal_barcode <- sample_list$normal_barcode[i]
-  cancer_file <- file_sample_map[barcode==cancer_barcode, filename]
-  normal_file <- file_sample_map[barcode==normal_barcode, filename]
-  if(!is.na(cancer_barcode)){
-    tmp <- fread(paste(source_file_dir, cancer_file, sep=""))
-    tmp$Sample <- cancer_barcode
-    cancer_list[[ cancer_barcode ]] <- tmp
-  }
-  if(!is.na(normal_barcode)){
-    tmp <- fread(paste(source_file_dir, normal_file, sep=""))
-    tmp$Sample <- normal_barcode
-    normal_list[[ normal_barcode]] <- tmp
-  }
+  tmp <- fread(
+    paste(
+      src_file_dir, file_sample_map[participants[i]]$filename, sep = ""
+    )
+  )
+  tmp$Sample <- participants[i]
+  cnvs_list[[participants[i]]] <- tmp
+  setTxtProgressBar(pb, i)
 }
+newline(verbose)
+exit(verbose)
 
-# Assign systematic names to data frames and save ------------------------------
-assign(paste(cancer_type, "_cnvl_cancer", sep=""), cancer_list)
-assign(paste(cancer_type, "_cnvl_normal", sep=""), normal_list)
-save(list = paste(cancer_type, "_cnvl_cancer", sep=""),
-     file = paste(output_dir, cancer_type, "_cnvl_cancer.Rdata", sep=""))
-save(list = paste(cancer_type, "_cnvl_normal", sep=""),
-     file = paste(output_dir, cancer_type, "_cnvl_normal.Rdata", sep=""))
+# Binding data to one data frame and transforming with CNTools -----------------
+enter(verbose, "Converting data to wide format")
+cat(verbose, "Binding list to one data.table")
+cnvs_bound <- rbindlist(cnvs_list)
+setnames(
+  cnvs_bound,
+  c("ID", "chrom", "loc.start", "loc.end", "n.probes", "seg.mean")
+)
+
+cat(verbose, "Converting from long to wide format")
+gene_info <- fread(
+  paste(
+    parsed_data_dir, cancer_type, "/info/hg19geneinfo.txt", sep=""
+  )
+)
+cnvs_seg <- CNSeg(cnvs_bound)
+cnvs_rs  <- getRS(
+  cnvs_seg,
+  by = "gene", imput = F, XY = T, what = "mean", geneMap = gene_info
+)
+cnvs_cancer <-data.table(rs(cnvs_rs))
+exit(verbose)
+
+# Save to .RDS file
+enter(verbose, "Saving files")
+cat(
+  verbose, "List format to ",
+  paste(output_dir, cancer_type, "_cnvl_cancer.Rds", sep = "")
+)
+saveRDS(
+  cnvs_list,
+  file = paste(output_dir, cancer_type, "_cnvl_cancer.Rds", sep = "")
+)
+cat(
+  verbose, "Wide format to ",
+  paste(output_dir, cancer_type, "_cnvs_cancer.Rds", sep = "")
+)
+saveRDS(
+  cnvs_cancer,
+  file = paste(output_dir, cancer_type, "_cnvs_cancer.Rds", sep = "")
+)
+exit(verbose)
